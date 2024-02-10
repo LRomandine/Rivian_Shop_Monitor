288f9ad82504161c7532f20b06cd8a569968da55
Levi Romandine <LRomandine@users.noreply.github.com>
2024-02-09 20:47:39 -0800

    Update rivian_shop_selenium_runner.sh

--- rivian_shop_selenium_runner.sh
+++ rivian_shop_selenium_runner.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# The username, password, email address, etc. are all obviously fake
 ./rivian_shop_selenium.py --username 'example@gmail.com' --password 'Password123' --email 'example@gmail.com' --zip '90210'
